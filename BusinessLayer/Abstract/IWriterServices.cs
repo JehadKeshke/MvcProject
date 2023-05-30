5f0f6dafef98370137146195e7bd68e422f1354a
GehadEddin <cihadeddink@gmail.vom>
2023-05-30 12:38:25 +0300

    Starting operations on the writers

---
+++ BusinessLayer/Abstract/IWriterServices.cs
@@ -0,0 +1,18 @@
+﻿using Entity_Layer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+   public  interface IWriterServices
+    {
+        List<Writer> GetList();
+        void AddWriter(Writer writer);
+        void DeleteWriter(Writer writer);
+        void UpdateWriter(Writer writer);
+        Writer GetByID(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using Entity_Layer.Concrete;
+using EntityAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterServices
+    {
+        IWriterDal _writerdal;
+
+        public WriterManager(IWriterDal writerdal)
+        {
+            _writerdal = writerdal;
+        }
+
+        public void AddWriter(Writer writer)
+        {
+            _writerdal.Insert(writer);
+
+        }
+
+        public void DeleteWriter(Writer writer)
+        {
+            _writerdal.Delete(writer);    
+        }
+
+        public Writer GetByID(int id)
+        {
+            return _writerdal.Get(x => x.WriterID == id);
+        }
+
+        public List<Writer> GetList()
+        {
+            return _writerdal.List();
+        }
+
+        public void UpdateWriter(Writer writer)
+        {
+            _writerdal.Update(writer);
+        }
+    }
+}
