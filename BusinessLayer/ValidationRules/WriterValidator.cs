f8a4a553be91a93dcd4e87056cb1067d77f452c5
GehadEddin <cihadeddink@gmail.vom>
2023-05-30 14:35:12 +0300

    Add writers to database

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entity_Layer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Please enter the writer name");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Please enter the writer surname");
+            RuleFor(x => x.About).NotEmpty().WithMessage("Please enter the discraption about writer");
+            RuleFor(x => x.WriterName).MinimumLength(3).WithMessage("Please enter at least three characters");
+            RuleFor(x => x.WriterName).MaximumLength(20).WithMessage("Please enter less than twenty characters");
+        }
+
+    }
+}
--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using Entity_Layer.Concrete;
 using EntityAccessLayer.EntityFramework;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,34 @@ namespace MvcProjeKampi.Controllers
             var WriterValue = wm.GetList();
             return View(WriterValue);
         }
-        
-     
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                wm.AddWriter(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
+
     }
 }
