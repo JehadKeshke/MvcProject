690ea27eb9d9ad7224e125df6298bad3b9d5fd9e
GehadEddin <cihadeddink@gmail.vom>
2023-07-08 19:45:12 +0300

    Create Gallery Page

---
+++ BusinessLayer/Concrete/GalleryManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using Entity_Layer.Concrete;
+using EntityAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class GalleryManager : IGalleryService
+    {
+        IGalleryDal _galleryDal;
+        public GalleryManager(IGalleryDal galleryDal)
+        {
+            _galleryDal = galleryDal;
+        }
+
+        public List<Gallery> GetList()
+        {
+            return _galleryDal.List();
+        }
+    }
+}
---
+++ EntityAccessLayer/EntityFramework/EfGalleryDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Concrete;
+using Entity_Layer.Concrete;
+using EntityAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityAccessLayer.EntityFramework
+{
+    public class EfGalleryDal : GenericRepository<Gallery>, IGalleryDal
+    {
+    }
+}
