a02fd0ea241c1a5053335b498c446c992314b30e
GehadEddin <cihadeddink@gmail.vom>
2023-07-20 15:53:52 +0300

    create Writer profile page for edit writer informations

--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Entity_Layer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,17 +16,43 @@ namespace MvcProjeKampi.Controllers
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
-
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator wv = new WriterValidator();
+        Context c = new Context();
         // GET: WriterPanel
-        
-        public ActionResult WriterProfile()
+
+        [HttpGet]
+        public ActionResult WriterProfile(int id = 0)
         {
+            string p = (string)Session["WriterMail"];
+            id = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+            var wirteridinfo = wm.GetByID(id);
+            return View(wirteridinfo);
+        }
+
+
+        [HttpPost]
+        public ActionResult WriterProfile(Writer p)
+        {
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                wm.UpdateWriter(p);
+                return RedirectToAction("MyHeading");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
         [Authorize]
         public ActionResult MyHeading(string p)
         {
-            Context c = new Context();
+            
             p = (string)Session["WriterMail"];
             var wirterinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
             var value = hm.GetListByWriter(wirterinfo);
